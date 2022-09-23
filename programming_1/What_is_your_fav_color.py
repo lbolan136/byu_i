883970e6642f92e94320edcbc89fd3da8ee611cc
lbolan136 <51797977+lbolan136@users.noreply.github.com>
2022-09-22 19:00:53 -0800

    Added the home page for wdd130.

---
+++ programming_1/What_is_your_fav_color.py
@@ -0,0 +1,25 @@
+fav_color = input("Please type your favorite color: ")
+#input favorite color
+print(fav_color)
+print()
+#whatelse do you like?
+second_fav_color = input("What is your second favorite color?: ")
+print(second_fav_color)
+print()
+#trying to have an interesting and somewhat varied conversation.
+hmm_interesting = input("Hmmm...interesting. What is your favorite place in the whole world?: ")
+print(hmm_interesting)
+print()
+#what is that new place (I'm a geographer!)
+print("Fascinating. Have you ever thought about going to the nearest place to your favorite place")
+print()
+#I like learning anbout new places, especially in preparation for the Foreign Service Officer Test
+new_place = ("What is that place again?: ")
+print(new_place)
+#have you ever heard about Polynesia?
+print("That's a neat place! My favorite place is Samoa.")
+print()
+#what if that person is from Micronesia or Melanesia? oh crap!
+polynesia = input("Do you have a favorite Pacific island?: ")
+print(polynesia)
+print("Well thanks for chatting. I really appreciate it. Have a nice day in that place.")
---
+++ programming_1/check_02_bolan.py
@@ -0,0 +1,8 @@
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+print(first_name)
+print()
+print(last_name)
+print()
+output = 'Your name is ' + last_name + ', ' + first_name + ',' + last_name + "."
+print(output)
