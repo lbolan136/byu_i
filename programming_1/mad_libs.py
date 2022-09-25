04b443b456a2828b996a902d3b32d7f4a074fca6
lbolan136 <51797977+lbolan136@users.noreply.github.com>
2022-09-25 14:52:04 -0800

    Added HTML pages and Mad Libs Python Script.

---
+++ programming_1/mad_libs.py
@@ -0,0 +1,19 @@
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+exclamation = input("verb: ")
+verb_ii = input("verb: ")
+verb_iii = input("verb: ")
+
+print("Please print the following:")
+print()
+
+print(adjective)
+print(animal)
+print(verb)
+print(exclamation)
+print(verb_ii)
+print(verb_iii)
+
+
+
