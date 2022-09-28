36f290b2b72a49ce002ecdc9316e754337929375
lbolan136 <51797977+lbolan136@users.noreply.github.com>
2022-09-27 20:04:08 -0800

    changed private to public

---
+++ programming_1/checkpoint_03.py
@@ -0,0 +1,20 @@
+age = int(input("How old are you? "))
+next_year_age = age + 1
+print(f"On your next birthday, you will be {next_year_age}.")
+amount_eggs = input("How many egg cartons do you have?: ")
+total_eggs = amount_eggs*12
+
+print(f"You have a total of {total_eggs} eggs")
+print()
+amount_cookies = int(input("How many cookies do you have?: "))
+
+amount_people = int(input("How many people are there?: "))
+print()
+per_capita_cookies = amount_cookies / amount_people
+print(f"Each person may have {per_capita_cookies} cookies.")
+
+
+
+
+
+
