d622a3a63f1bfc4b963512e4c1b8bb158108de4c
lbolan136 <51797977+lbolan136@users.noreply.github.com>
2022-09-27 19:50:21 -0800

    added and finished mad_libs task

--- programming_1/mad_libs.py
+++ programming_1/mad_libs.py
@@ -1,3 +1,4 @@
+print("Please fill out the following for madlibs:")
 adjective = input("adjective: ")
 animal = input("animal: ")
 verb = input("verb: ")
@@ -17,3 +18,7 @@ print(verb_iii)
 
 
 
+print(f"I was walking down the trail behind my house when I saw a {adjective} {animal}. I was so suprised, I jumped out of my skin!")
+print(f"Holy {exclamation}! What's gotten into you?")
+print(f"I started to {verb} from the {adjective} {animal}. But the {animal} started to {verb_ii} on me. There was no escape. I consoled myself with my last memory of my ice cream sandwhich eating contest with my mom.")
+print("But suddenly, as the {animal} caught up to me, the {adjective} {animal} began to {verb_iii} me. It was full of passion.")
